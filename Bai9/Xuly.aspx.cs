32a5f93591cd13792bb2a2b7b689049d2b7922f3
Hieu <hieun0981@gmail.com>
2024-10-09 15:01:33 +0700

    edit

--- Bai9/Xuly.aspx.cs
+++ Bai9/Xuly.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace Bai9
 {
@@ -11,21 +13,25 @@ namespace Bai9
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                string ten = Request.Form["txtTen"];
-                string hang = Request.Form["txtHang"] ;
-                string ngay = Request.Form["dNgaySX"];
-                string gia = Request.Form["fGia"];
-                Response.Write("Ten: "+ ten + "<br>Hang: " + hang + "<br>Ngay: " + ngay + "<br>Gia: " + gia );
 
-            }
-            /*Response.ContentType = "text/xml";
-            string ten = "<ten>" + Request.Form["txtTen"] + "</ten>";
-            string hang = "<hang>" + Request.Form["txtHang"] + "</hang>";
-            string ngay = "<ngaysx>" + Request.Form["dNgaySX"] + "</ngaysx>";
-            string gia = "<gia>" + Request.Form["fGia"] + "</gia>";
-            Response.Write("<body>" + ten + hang + ngay + gia + "</body>");*/
+            /*string ten = Request.Form["txtTen"];
+            string hang = Request.Form["txtHang"];
+            string ngay =  Request.Form["dNgaySX"];
+            string gia =  Request.Form["fGia"];
+            writeXML()*/
+            /*string path = @"D://hihi.xml";*/
+
+
+
+            Response.ContentType = "text/xml";
+            var ten = "<ten>" + Request.Form["txtTen"] + "</ten>";
+            var hang = "<hang>" + Request.Form["txtHang"] + "</hang>";
+            var ngay = "<ngaysx>" + Request.Form["dNgaySX"] + "</ngaysx>";
+            var gia = "<gia>" + Request.Form["fGia"] + "</gia>";
+            Response.Write("<?xml version='1.0' encoding='utf-8'?>");
+            Response.Write("<body>" + ten + hang + ngay + gia + "</body>");
+
         }
+        
     }
 }
